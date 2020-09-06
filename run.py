661eb2e28370e8fde1a7defdf7ec0dab154c5462
Noble Mungu <mungunoble@gmail.com>
2020-09-06 16:14:38 +0300

    add the navigation codes

--- run.py
+++ run.py
@@ -53,3 +53,18 @@ def copy_credential(site_name):
     Function to copy a credentials details to the clipboard
     '''
     return Credential.copy_credential(site_name)
+
+def main():
+	print('')
+	print('Hello!Welcome to Password locker.')
+	while True:
+		print(' ')
+		print('-'*60)
+		print('Use these codes to navigate:\n ca-Create an account \n lg-login \n ex-Exit')
+		short_code = input('Enter a choice').lower().strip()
+		if short_code == 'ex':
+			break
+
+if __name__ == '__main__':
+	main()
+
